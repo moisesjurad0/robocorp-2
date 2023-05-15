6e75d0b7ceb503d355d706335d6280693e10c34a
Moisés Jurado <moises003@gmail.com>
2023-05-15 12:41:45 -0500

    copied code from https://robocorp.com/docs/development-guide/getting-started/simple-example-robot#the-robot-written-in-python

--- task.py
+++ task.py
@@ -1,9 +1,25 @@
-"""Template robot with Python."""
+# https://robocorp.com/docs/development-guide/getting-started/simple-example-robot#the-robot-written-in-python
+
+from RPA.Browser.Selenium import Selenium
+from RPA.FileSystem import FileSystem
+
+browser = Selenium()
 
 
-def minimal_task():
-    print("Done.")
+def store_web_page_content():
+    browser.open_available_browser("https://robotframework.org/")
+    text = browser.get_text("css:body")
+    FileSystem().create_directory("./output/")  # improvement
+    FileSystem().create_file("output/text.txt", text, overwrite=True)
+    browser.screenshot("css:h1", "output/screenshot.png")
+
+
+def main():
+    try:
+        store_web_page_content()
+    finally:
+        browser.close_browser()
 
 
 if __name__ == "__main__":
-    minimal_task()
+    main()
